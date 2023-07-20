5bd93eed48c2b72a8b29dd6967b80816ac9a111f
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-07-21 00:08:29 +0300

    commit

--- 002eksikveriler.py
+++ 002eksikveriler.py
@@ -7,22 +7,14 @@ veriler=pd.read_csv('eksikveriler.csv')
 
 print(veriler)
 
-boy = veriler[['boy']]
-print(boy)
-boykilo = veriler[['boy','kilo']]
-print(boykilo)
-
-x=10
-
-class insan:
-    boy=180
-    def kosmak(self,b):
-        return b+x
-    
-ali = insan()
-print(ali.boy)
-print(ali.kosmak(90))
 
 #eksik veriler
 #sci kit learn
 from sklearn.impute import SimpleImputer
+imputer= SimpleImputer(missing_values=np.nan,strategy='mean')
+Yas=veriler.iloc[:,1:4].values
+print(Yas)
+imputer=imputer.fit(Yas[:,1:4])
+
+Yas[:,1:4]=imputer.transform(Yas[:,1:4])
+print(Yas)
