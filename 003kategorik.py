6e4b392f8965fc995fe3c669785f75dec00d7758
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-07-22 23:10:23 +0300

    commit

--- 003kategorik.py
+++ 003kategorik.py
@@ -2,22 +2,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-veriler=pd.read_csv('eksikveriler.csv')
+veriler=pd.read_csv('veriler.csv')
 # print(veriler)
-
-#eksik veriler
-#sci kit learn
-from sklearn.impute import SimpleImputer
-imputer= SimpleImputer(missing_values=np.nan,strategy='mean')
-Yas=veriler.iloc[:,1:4].values
-# print(Yas)
-imputer=imputer.fit(Yas[:,1:4])
-
-Yas[:,1:4]=imputer.transform(Yas[:,1:4])
-# print(Yas)
 
 ulke= veriler.iloc[:,0:1].values
 print(ulke)
+
 from sklearn import preprocessing
 le=preprocessing.LabelEncoder()
 
