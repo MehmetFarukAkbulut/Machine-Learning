d78305c13289fca2d1eff06dc5499af26ec5bb94
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-07-22 22:23:17 +0300

    commit

---
+++ 003kategorik.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+veriler=pd.read_csv('eksikveriler.csv')
+# print(veriler)
+
+#eksik veriler
+#sci kit learn
+from sklearn.impute import SimpleImputer
+imputer= SimpleImputer(missing_values=np.nan,strategy='mean')
+Yas=veriler.iloc[:,1:4].values
+# print(Yas)
+imputer=imputer.fit(Yas[:,1:4])
+
+Yas[:,1:4]=imputer.transform(Yas[:,1:4])
+# print(Yas)
+
+ulke= veriler.iloc[:,0:1].values
+print(ulke)
