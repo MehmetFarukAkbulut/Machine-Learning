f7524ca5e501f3b69516390221d2df34a48dd0ea
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-07-24 23:41:25 +0300

    commit

--- 003kategorik.py
+++ 003kategorik.py
@@ -51,3 +51,12 @@ print(s2)
 from sklearn.model_selection import train_test_split
 
 x_train,x_test,y_train,y_test=train_test_split(s,sonuc3,test_size=0.33,random_state=0) 
+
+from sklearn.preprocessing import StandardScaler
+
+sc=StandardScaler()
+
+X_train=sc.fit_transform(x_train)
+X_test=sc.fit_transform(x_test)
+Y_train=sc.fit_transform(y_train)
+Y_test=sc.fit_transform(y_test)
